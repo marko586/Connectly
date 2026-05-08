0a2089f649b0042d58a2eb7976b5c599568d37ca
Marko Sysak <markosysak@gmail.com>
2026-05-08 16:11:12 +0200

    Debugging OTP

--- Connectly/utils.py
+++ Connectly/utils.py
@@ -14,4 +14,4 @@ def send_otp_email(user_email, otp_code):
     from_email = settings.EMAIL_HOST_USER
     recipient_list = [user_email]
 
-    send_mail(subject, message, from_email, recipient_list)
+    send_mail(subject, message, from_email, recipient_list, fail_silently=False)
